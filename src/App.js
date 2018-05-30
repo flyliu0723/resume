7deab24d105f8aa6008b0e6cc525d0da455defd6
刘宇飞 <liuyufei@tongtongmall.com>
2018-05-30 17:03:37 +0800

    begin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
+// import logo from '../public/logo2.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="header">
-          
+          {/* <img className="logo" src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/483/868140483_80f7996c-0044-4232-bed9-8b44e1dc1632.png?cb=1527665535" alt=""/> */}
+          <div className="links">
+            <a className="github" href="https://github.com/flyliu0723"></a>
+            <a className="blog" href="https://flyliu0723.github.io/flyblog/"></a>
+          </div>
+          <img className="logo" src={require('./images/logo2.png')} alt=""/>
+          <div className="down">
+            <button>DOWNLOAD</button>
+          </div>
+        </div>
+        <div className="header-img">
+          <img src="https://avatars2.githubusercontent.com/u/15263326?s=460&v=4" alt=""/>
         </div>
       </div>
     );
